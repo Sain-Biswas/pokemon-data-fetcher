3e7a1b9c3b63165a1cc49ec52eac51b3fb51884d
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-28 18:24:29 +0530

    chore: update setup.sh to provide more context

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 rm -rf migrations pokemon.db
 
-bun db:generate
-bun db:migrate
+# bun db:generate
+# bun db:migrate
+
+bun db:push
+
+echo "Starting data fetching"
 
 bun run src/01-group-fetcher.ts
+echo "Group 1 done"
+
 bun run src/02-group-fetcher.ts
+echo "Group 2 done"
+
 bun run src/03-group-fetcher.ts
+echo "Group 3 done"
+
 bun run src/04-group-fetcher.ts
+echo "Group 4 done"
+
+echo "Generation complete"
