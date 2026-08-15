4b66f2bada6182807e68472702723aff26ded816
Sain-Biswas <sain.biswas614@gmail.com>
2026-08-15 20:41:50 +0530

    refactor: removed unnamed from files to match naming used in pokeapi docs

--- src/validators/resource-lists/api-resource-list.zod.ts
+++ src/validators/resource-lists/api-resource-list.zod.ts
@@ -2,7 +2,7 @@ import { APIResourceZodSchema } from "~/validators/common/api-resource.zod";
 
 import { z } from "zod";
 
-export const UnnamedAPIResourceListZodSchema = z.object({
+export const APIResourceListZodSchema = z.object({
 	count: z.number().describe("The total number of resources available from this API."),
 	results: z.array(APIResourceZodSchema).describe("A list of unnamed API resources.")
 });
---
+++ test/validators/resource-lists/api-resource-list.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from "bun:test";
+import { APIResourceListZodSchema } from "~/validators/resource-lists/api-resource-list.zod";
+import { fetch } from "bun";
+import { z } from "zod";
+
+const fetchEvolutionChainResourceList = async (): Promise<unknown> => {
+	const response = await fetch("https://pokeapi.co/api/v2/evolution-chain");
+
+	if (!response.ok) {
+		throw new Error("API Resource List - NETWORK CONNECTION FAILED");
+	}
+
+	return (await response.json()) as unknown;
+};
+
+describe("API Resource List", () => {
+	it("Should match the zod schema - APIResourceListZodSchema", async () => {
+		const data = await fetchEvolutionChainResourceList(),
+			parsed = APIResourceListZodSchema.safeParse(data);
+
+		if (!parsed.success) {
+			console.error("APIResourceListZodSchema Parse Error:", z.treeifyError(parsed.error));
+		}
+
+		expect(parsed.success).toBe(true);
+	});
+});
