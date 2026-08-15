9252384430c150d5831d14016ff5b7dee5031b04
Sain-Biswas <sain.biswas614@gmail.com>
2026-08-15 19:02:09 +0530

    build: update oxfmt config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig } from "oxfmt"
+import { defineConfig } from "oxfmt";
 
 export default defineConfig({
-    arrowParens: "always",
-    bracketSpacing: true,
-    endOfLine: "lf",
     ignorePatterns: ["dist/**", "build/**", "coverage/**", "node_modules/**", "bun.lock", "bun.lockb"],
-    insertFinalNewline: true,
     printWidth: 120,
-    semi: false,
-    singleQuote: false,
     sortImports: true,
-    sortPackageJson: true,
     tabWidth: 4,
-    trailingComma: "all",
-    useTabs: false,
-})
+    trailingComma: "none",
+    useTabs: true,
+});
